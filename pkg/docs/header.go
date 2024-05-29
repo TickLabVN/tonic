2dc92abeecc83b10addfb969326f25506f92496b
David Tran <tranhuyducseven@gmail.com>
2024-05-29 16:18:51 +0700

    feat: more schema

---
+++ pkg/docs/header.go
@@ -0,0 +1,32 @@
+package docs
+
+// The Header Object follows the structure of the Parameter Object with the following changes:
+
+// name MUST NOT be specified, it is given in the corresponding headers map.
+// in MUST NOT be specified, it is implicitly in header.
+// All traits that are affected by the location MUST be applicable to a location of header (for example, style).
+
+// type Parameter struct {
+// 	Name        string `json:"name,omitempty" validate:"required"`                     //TODO: verify if this is path item object
+// 	In          string `json:"in,omitempty" validate:"oneof=query header path cookie"` //TODO: verify if this is path item object
+// 	Description string `json:"description,omitempty"`
+// 	Required    bool   `json:"required,omitempty" validate:"required"` //TODO: verify
+// 	Deprecated  bool   `json:"deprecated,omitempty"`
+// 	AllowEmpty  bool   `json:"allowEmptyValue,omitempty"`
+// }
+
+type Header struct {
+	Description string `json:"description,omitempty"`
+	Required    bool   `json:"required,omitempty" validate:"required"` //TODO: verify
+	Deprecated  bool   `json:"deprecated,omitempty"`
+	AllowEmpty  bool   `json:"allowEmptyValue,omitempty"`
+
+	Style         string              `json:"style,omitempty" validate:"oneof=matrix label form simple spaceDelimited pipeDelimited deepObject"`
+	Explode       bool                `json:"explode,omitempty"`
+	AllowReserved bool                `json:"allowReserved,omitempty"`
+	Schema        *Schema             `json:"schema,omitempty"`
+	Example       interface{}         `json:"example,omitempty"`
+	Examples      map[string]*Example `json:"examples,omitempty"`
+
+	Content map[string]*MediaType `json:"content,omitempty"`
+}
---
+++ pkg/docs/link.go
@@ -0,0 +1,19 @@
+package docs
+
+// Field Name	Type	Description
+// operationRef	string	A relative or absolute URI reference to an OAS operation. This field is mutually exclusive of the operationId field, and MUST point to an Operation Object. Relative operationRef values MAY be used to locate an existing Operation Object in the OpenAPI definition. See the rules for resolving Relative References.
+// operationId	string	The name of an existing, resolvable OAS operation, as defined with a unique operationId. This field is mutually exclusive of the operationRef field.
+// parameters	Map[string, Any | {expression}]	A map representing parameters to pass to an operation as specified with operationId or identified via operationRef. The key is the parameter name to be used, whereas the value can be a constant or an expression to be evaluated and passed to the linked operation. The parameter name can be qualified using the parameter location [{in}.]{name} for operations that use the same parameter name in different locations (e.g. path.id).
+// requestBody	Any | {expression}	A literal value or {expression} to use as a request body when calling the target operation.
+// description	string	A description of the link. CommonMark syntax MAY be used for rich text representation.
+// server	Server Object	A server object to be used by the target operation.
+
+// TODO: TODO: Parametters
+type Link struct {
+	OperationRef string                 `json:"operationRef,omitempty"`
+	OperationID  string                 `json:"operationId,omitempty"`
+	Parameters   map[string]interface{} `json:"parameters,omitempty"`
+	RequestBody  interface{}            `json:"requestBody,omitempty"`
+	Description  string                 `json:"description,omitempty"`
+	Server       *Server                `json:"server,omitempty"`
+}
