d3b106c743478b7231bb2ba9070e14694a4f9f06
Nguyen Phuc Vinh <npvinh0507@gmail.com>
2024-06-19 11:57:56 +0700

    test(validate): add template test files

---
+++ pkg/parser/validate_format_test.go
@@ -0,0 +1,258 @@
+package parser_test
+
+import "testing"
+
+// Format validate tags: https://github.com/go-playground/validator?tab=readme-ov-file#format
+
+func TestValidate_Format_Base64(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_Base64URL(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_Base64RawURL(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_BIC(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_BCP47LanguageTag(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_BTCAddr(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_BTCAddrBech32(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_CreditCard(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_MongoDB(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_MongoDBConnectionString(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_Cron(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_SpiceDB(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_Datetime(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_E164(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_Email(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_EthAddr(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_Hexadecimal(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_Hexcolor(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_HSL(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_HSLA(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_HTML(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_HTMLEncoded(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_ISBN(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_ISBN10(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_ISBN13(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_ISSN(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_ISO31661Alpha2(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_ISO31661Alpha3(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_ISO31661AlphaNumeric(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_ISO31662(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_ISO4217(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_JSON(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_JWT(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_Latitude(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_Longitude(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_LuhnChecksum(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_PostcodeISO31661Alpha2(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_PostcodeISO31661Alpha2Field(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_RGB(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_RGBA(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_SSN(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_Timezone(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_UUID(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_UUID3(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_UUID3RFC4122(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_UUID4(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_UUID4RFC4122(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_UUID5(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_UUID5RFC4122(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_UUIDRFC4122(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_MD4(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_MD5(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_SHA256(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_SHA384(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_SHA512(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_RIPEMD128(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_RIPEMD160(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_TIGER128(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_TIGER160(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_TIGER192(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_Semver(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_ULID(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Format_CVE(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
---
+++ pkg/parser/validate_networks_test.go
@@ -0,0 +1,111 @@
+package parser_test
+
+import "testing"
+
+func TestValidate_Networks_Cidr(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Cidrv4(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Cidrv6(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Datauri(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Fqdn(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Hostname(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Hostname_port(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Hostname_rfc1123(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Ip(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Ip4_addr(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Ip6_addr(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Ip_addr(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Ipv4(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Ipv6(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Mac(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Tcp4_addr(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Tcp6_addr(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Tcp_addr(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Udp4_addr(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Udp6_addr(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Udp_addr(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Unix_addr(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Uri(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Url(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Http_url(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Url_encoded(t *testing.T) {
+	t.Skip("Not implemented")
+}
+
+func TestValidate_Networks_Urn_rfc2141(t *testing.T) {
+	t.Skip("Not implemented")
+}
