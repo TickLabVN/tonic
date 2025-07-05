daad95a3a1550aca91619a75fa5a8b15dbd9fe2f
HamidReza Ranjbar <hamidreza.ranjbar@digikala.com>
2025-07-05 23:43:40 +0330

    fix: avoid manually appending url prefixes for gin adapter

--- adapters/gin/route.go
+++ adapters/gin/route.go
@@ -63,25 +63,25 @@ func AddRoute[D any, R any](spec *docs.OpenApi, g gin.IRoutes, route Route) {
 	pathItem := docs.PathItemObject{}
 	switch route.Method {
 	case http.MethodGet:
-		g.GET(path, route.Handlers...)
+		g.GET(route.Path, route.Handlers...)
 		pathItem.Get = &op
 	case http.MethodPost:
-		g.POST(path, route.Handlers...)
+		g.POST(route.Path, route.Handlers...)
 		pathItem.Post = &op
 	case http.MethodPut:
-		g.PUT(path, route.Handlers...)
+		g.PUT(route.Path, route.Handlers...)
 		pathItem.Put = &op
 	case http.MethodPatch:
-		g.PATCH(path, route.Handlers...)
+		g.PATCH(route.Path, route.Handlers...)
 		pathItem.Patch = &op
 	case http.MethodDelete:
-		g.DELETE(path, route.Handlers...)
+		g.DELETE(route.Path, route.Handlers...)
 		pathItem.Delete = &op
 	case http.MethodOptions:
-		g.OPTIONS(path, route.Handlers...)
+		g.OPTIONS(route.Path, route.Handlers...)
 		pathItem.Options = &op
 	case http.MethodHead:
-		g.HEAD(path, route.Handlers...)
+		g.HEAD(route.Path, route.Handlers...)
 		pathItem.Head = &op
 	default:
 		fmt.Printf("Unsupported HTTP method: %s\n", route.Method)
